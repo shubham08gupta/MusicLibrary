eeecec58a0a9dd17c9bb140661b7d1dd952880fe
shubham.gupta <shubham.gupta@unifa-e.com>
2021-06-13 17:51:26 +0900

    add search view

--- app/src/main/java/com/strings/attached/musiclibrary/ui/search/SearchFragment.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/ui/search/SearchFragment.kt
@@ -1,6 +1,44 @@
 package com.strings.attached.musiclibrary.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import com.strings.attached.musiclibrary.R
+import com.strings.attached.musiclibrary.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment(R.layout.fragment_search)
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.artistsSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrEmpty()) {
+                    // TODO: 2021/06/13 search for the artist
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean = false
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
