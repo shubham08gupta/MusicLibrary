d0b39ae3dda316fc02181d663f08731068d25865
shubham.gupta <shubham.gupta@unifa-e.com>
2021-06-13 15:41:11 +0900

    add search icon | navigate to the search screen on icon click

--- app/src/main/java/com/strings/attached/musiclibrary/ui/home/HomeFragment.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/ui/home/HomeFragment.kt
@@ -1,6 +1,43 @@
 package com.strings.attached.musiclibrary.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.strings.attached.musiclibrary.R
+import com.strings.attached.musiclibrary.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search -> {
+                    findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSearchFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
