0401cacaf74f202de557dcbee51b7c7e918f02ee
GUPTA SHUBHAM <shubham.gupta@unifa-e.com>
2021-06-19 20:51:59 +0900

    add a ViewModel to fetch the album details and emit it using a StateFlow

--- app/src/main/java/com/strings/attached/musiclibrary/ui/album/detail/AlbumDetailViewModel.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/ui/album/detail/AlbumDetailViewModel.kt
@@ -2,13 +2,35 @@ package com.strings.attached.musiclibrary.ui.album.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.strings.attached.musiclibrary.data.album.AlbumRepository
+import com.strings.attached.musiclibrary.model.album.AlbumDetail
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class AlbumDetailViewModel @AssistedInject constructor(
+    albumRepository: AlbumRepository,
     @Assisted("artistName") private val artistName: String,
     @Assisted("albumName") private val albumName: String,
 ) : ViewModel() {
+
+    private val _albumDetailUiState: MutableStateFlow<AlbumDetailUiState> =
+        MutableStateFlow(AlbumDetailUiState.Loading)
+    val albumDetailUiState: StateFlow<AlbumDetailUiState> get() = _albumDetailUiState
+
+    init {
+        viewModelScope.launch {
+            val result = albumRepository.getAlbumDetail(artistName, albumName)
+            result.fold({
+                _albumDetailUiState.value = AlbumDetailUiState.Success(it)
+            }, {
+                _albumDetailUiState.value = AlbumDetailUiState.Error(it)
+            })
+        }
+    }
 
     /***
      * Use an assisted factory to inject manual parameters
@@ -33,4 +55,10 @@ class AlbumDetailViewModel @AssistedInject constructor(
             }
         }
     }
+}
+
+sealed class AlbumDetailUiState {
+    object Loading : AlbumDetailUiState()
+    data class Success(val album: AlbumDetail) : AlbumDetailUiState()
+    data class Error(val throwable: Throwable) : AlbumDetailUiState()
 }
