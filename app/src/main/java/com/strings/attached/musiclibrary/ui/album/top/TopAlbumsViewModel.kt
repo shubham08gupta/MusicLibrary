629e9dba6ade691a508d992dfa7f725509e6a588
GUPTA SHUBHAM <shubham.gupta@unifa-e.com>
2021-06-19 16:22:53 +0900

    add a ViewModel to fetch the top albums of an artist by their name

--- app/src/main/java/com/strings/attached/musiclibrary/ui/album/top/TopAlbumsViewModel.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/ui/album/top/TopAlbumsViewModel.kt
@@ -2,12 +2,23 @@ package com.strings.attached.musiclibrary.ui.album.top
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.strings.attached.musiclibrary.data.album.AlbumRepository
+import com.strings.attached.musiclibrary.model.album.Album
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.Flow
 
 class TopAlbumsViewModel @AssistedInject constructor(
+    albumRepository: AlbumRepository,
     @Assisted("artistName") private val artistName: String,
 ) : ViewModel() {
+
+    val topAlbumsFlow: Flow<PagingData<Album>> =
+        albumRepository.getTopAlbumsByArtistName(artistName)
+            .cachedIn(viewModelScope)
 
     /***
      * Use an assisted factory to inject manual parameters
