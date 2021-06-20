f5d10eccc8f20c6b2e8cd05ce99c8331d9b69e15
GUPTA SHUBHAM <shubham.gupta@unifa-e.com>
2021-06-20 18:47:23 +0900

    add Room annotations to the models to save them in the DB

--- app/src/main/java/com/strings/attached/musiclibrary/model/album/AlbumDetail.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/model/album/AlbumDetail.kt
@@ -1,23 +1,41 @@
 package com.strings.attached.musiclibrary.model.album
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.strings.attached.musiclibrary.model.artist.Image
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 /***
  * The details of an [Album] has a different response format. Therefore, we need to create a
- * different class for details.
+ * different class for details. Creates its own primary key as the server does not returns
+ * a unique id.
  */
+@Entity
 @Serializable
 data class AlbumDetail(
-    val name: String,
+    @PrimaryKey(autoGenerate = true) val albumId: Long = 0,
+    @SerialName("name") val albumName: String,
     @SerialName("artist") val artistName: String,
-    val url: String,
-    val image: List<Image> = emptyList(),
-    val listeners: String,
+    @SerialName("url") val url: String,
+    @SerialName("image") @Ignore val image: List<Image> = emptyList(),
+    @Transient val albumImageUrl: String? = null, // choose an image from List<Image> to save in DB
+    @SerialName("listeners") val listeners: String,
     @SerialName("playcount") val playCount: Int,
-    val tracks: Tracks? = null
-)
+    @SerialName("tracks") @Ignore val tracks: Tracks? = null
+) {
+    constructor(
+        albumId: Long,
+        albumName: String,
+        artistName: String,
+        url: String,
+        albumImageUrl: String?,
+        listeners: String,
+        playCount: Int
+    ) : this(albumId, albumName, artistName, url, emptyList(), albumImageUrl, listeners, playCount)
+}
 
 @Serializable
 data class Tracks(
--- app/src/main/java/com/strings/attached/musiclibrary/model/album/Track.kt
+++ app/src/main/java/com/strings/attached/musiclibrary/model/album/Track.kt
@@ -1,11 +1,30 @@
 package com.strings.attached.musiclibrary.model.album
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = AlbumDetail::class,
+            parentColumns = ["albumId"],
+            childColumns = ["trackAlbumId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE,
+        )
+    ],
+    indices = [Index(value = ["trackAlbumId"])]
+)
 @Serializable
 data class Track(
-    @SerialName("name") var name: String,
-    @SerialName("url") var url: String,
-    @SerialName("duration") var duration: String
+    @PrimaryKey(autoGenerate = true) val trackId: Long = 0,
+    @Transient var trackAlbumId: Long = -1, // acts as a Foreign Key
+    @SerialName("name") val name: String,
+    @SerialName("url") val url: String,
+    @SerialName("duration") val duration: String
 )
