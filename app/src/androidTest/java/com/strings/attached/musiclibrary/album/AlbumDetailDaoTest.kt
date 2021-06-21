bc6e043cefe53d55c1993ec5c975c0a3392904cd
GUPTA SHUBHAM <shubham.gupta@unifa-e.com>
2021-06-21 22:54:24 +0900

    fix deprecated import

--- app/src/androidTest/java/com/strings/attached/musiclibrary/album/AlbumDetailDaoTest.kt
+++ app/src/androidTest/java/com/strings/attached/musiclibrary/album/AlbumDetailDaoTest.kt
@@ -9,9 +9,9 @@ import com.strings.attached.musiclibrary.db.AlbumDao
 import com.strings.attached.musiclibrary.db.AppDatabase
 import com.strings.attached.musiclibrary.db.TrackDao
 import com.strings.attached.musiclibrary.util.TestUtil
-import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
